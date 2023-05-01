cc36c0aa810e4f2344929357b5086c8d9481bb22
kacwojdev <wojnowski.kacper@gmail.com>
2023-05-01 19:07:18 +0200

    created app theme and types for it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import styled, {ThemeProvider} from "styled-components";
+import {theme} from "../lib/styles";
 export const App = () => {
     return (
-        <div>
-            Hello world!
-        </div>
+        <ThemeProvider theme={theme}>
+            <AppContainer>
+                Hello world!
+            </AppContainer>
+        </ThemeProvider>
     )
 }
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: ${({theme}) => theme.colors.background};
+`
